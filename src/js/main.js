515c18acb981e41fdf2c1726d79ce134d76ebf9c
Alexandr <ordamaleus@gmail.com>
2023-01-29 07:38:05 +0500

    Troubles with Swiper

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+// $(function(){
+//     $('.slider_inner').slick();
+// })
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+Swiper.use({ Navigation, Pagination });
+
+const swiper = new Swiper('.swiper', {
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
