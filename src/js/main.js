f0122b0683f2621854e6705ff9e91bc5567b96a6
Alexandr <ordamaleus@gmail.com>
2023-03-26 09:37:53 +0500

    Number of pages on JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,29 @@
-
-
 //СлайдШоу
 
-var slideIndex = 1;
+
+let count = 1;
+
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Вперед/назад элементы управления
 function plusSlides(n) {
   showSlides(slideIndex += n);
+  //Счетчик переключения страницы
+  count += 1;
+  if (count > 3) {
+    count = 1;
+  }
+  click.innerHTML =  count;
+}
+function minusSlides(n) {
+  showSlides(slideIndex -= n);
+  //Счетчик переключения страницы
+  count -= 1;
+  if (count < 1) {
+    count = 3;
+  }
+  click.innerHTML = count;
 }
 
 // Элементы управления миниатюрами изображений
@@ -16,17 +32,35 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var lines = document.getElementsByClassName("line");
+  let slides = document.getElementsByClassName("mySlides");
+  let lines = document.getElementsByClassName("line");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
-  for (i = 0; i < lines.length; i++) {
+  for (let i = 0; i < lines.length; i++) {
       lines[i].className = lines[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";
   lines[slideIndex-1].className += " active";
 }
+
+
+
+
+
+// let click = document.getElementById("click");
+
+// let next = document.getElementById("next");
+// let prev = document.getElementById("prev");
+// next.onclick = function() {
+//   count += 1;
+//   click.innerHTML =  count;
+//   console.log(click)
+// };
+// prev.onclick = function() {
+//   count -= 1;
+//   click.innerHTML =  count;
+  
+// };
